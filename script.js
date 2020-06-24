ef3ca9659499ec41b3c37d1e35e749b121a6bda3
Tomas <tomasfreire@gmail.com>
2020-06-24 20:25:15 -0300

    set new time and style updates

--- script.js
+++ script.js
@@ -2,16 +2,37 @@ const timer = document.getElementById("timer");
 const startButton = document.getElementById("start-button");
 const pauseButton = document.getElementById("pause-button");
 const clearButton = document.getElementById("clear-button");
+const newTimeInput = document.getElementById("set-time-input");
+const newTimeForm = document.getElementById("set-time-form")
+const audio = new Audio("./purge.mp3");
 
+newTimeForm.addEventListener("submit", newTimeSubmit)
+
+let newTime;
 let isPaused = false;
 let interval;
 let current = 0;
-let count = 1500 - current;
 
+function newTimeSubmit(event){
+  event.preventDefault()
+  if(newTimeInput.value > 99 || newTimeInput.value < 1){
+    return
+  } else {
+    newTime = (newTimeInput.value * 60)
+  }
+  newTimeInput.value = ""
+  return timer.innerHTML = `${
+    Math.floor(newTime / 60) < 10
+      ? "0" + Math.floor(newTime / 60)
+      : Math.floor(newTime / 60)
+  }:${
+    Math.floor(newTime % 60) < 10
+      ? "0" + Math.floor(newTime % 60)
+      : Math.floor(newTime % 60)
+  }`;
+}
 
-
-const audio = new Audio("./purge.mp3");
-
+let count = (newTime || 1500) - current;
 
 function start() {
   isPaused = false;
@@ -30,15 +51,15 @@ function start() {
           : Math.floor(count % 60)
       }`;
     }
-    if(count < 0){
-      audio.play()
+    if (count == 0) {
+      audio.play();
     }
   }, 1000);
 }
 
 function pause() {
   isPaused = true;
-  clearInterval(interval)
+  clearInterval(interval);
   current = count;
   startButton.disabled = false;
   pauseButton.disabled = true;
@@ -48,7 +69,7 @@ function clearAll() {
   clearInterval(interval);
   count = 1500;
   isPaused = false;
-  timer.innerHTML = "25:00"
+  timer.innerHTML = "25:00";
   startButton.disabled = false;
   pauseButton.disabled = true;
 }
