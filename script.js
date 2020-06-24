0d6510edc90db822f61779063de958a792e06593
Tomas <tomasfreire@gmail.com>
2020-06-24 18:03:39 -0300

    added pause and clear functionalities

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 const timer = document.getElementById("timer");
-const button = document.getElementById("button");
+const startButton = document.getElementById("start-button");
+const pauseButton = document.getElementById("pause-button");
+const clearButton = document.getElementById("clear-button");
 
 let count = 1500;
-
+let isPaused = false;
 let interval;
 
 const audio = new Audio("./purge.mp3");
 
+
 function start() {
-  if (count < 1500) {
-    clearInterval(interval);
-    button.innerHTML = "Start";
-  } else {
-    button.innerHTML = "Stop";
-    interval = setInterval(setHtml, 1000);
-  }
+  isPaused = false;
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+  interval = setInterval(() => {
+    if (!isPaused && count > 0) {
+      count--;
+      timer.innerHTML = `${
+        Math.floor(count / 60) < 10
+          ? "0" + Math.floor(count / 60)
+          : Math.floor(count / 60)
+      }:${
+        Math.floor(count % 60) < 10
+          ? "0" + Math.floor(count % 60)
+          : Math.floor(count % 60)
+      }`;
+    }
+  }, 1000);
 }
 
-function setHtml() {
-  count--;
-  if (count < 0) {
-    button.innerHTML = "Start";
-    audio.play();
-    clearInterval(interval);
-  } else {    
-    timer.innerHTML = `${Math.floor(count / 60)}:${
-      Math.floor(count % 60) < 10
-        ? "0" + Math.floor(count % 60)
-        : Math.floor(count % 60)
-    }`;
-  }
+function pause() {
+  isPaused = true;
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+}
+
+function clearAll() {
+  clearInterval(interval);
+  count = 1500;
+  isPaused = false;
+  timer.innerHTML = "25:00"
+  startButton.disabled = false;
+  pauseButton.disabled = true;
 }
